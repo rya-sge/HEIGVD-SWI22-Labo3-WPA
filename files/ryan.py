3ebee553f8c9a57cb985a6789e2fb222482b88da
rya-sge <ryan.sauge@heig-vd.ch>
2022-05-01 15:37:18 +0200

    get all information

--- files/ryan.py
+++ files/ryan.py
@@ -60,40 +60,44 @@ APmac = ""
 Clientmac = ""
 print(len(wpa))
 ANonce = ""
+cpt = 0
+mic_to_test = ""
 for pkt in wpa:
     if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
         if pkt.type == 0 and pkt.subtype == 8:
-            # ap_list.append((pkt.ssid, pkt.addr2))
             ssid = pkt.info
-            print(type(ssid))
+            print("SSID" , ssid)
             APmac = bytes(pkt.addr2, "utf-8")
-            print(type(APmac))
-            # return "Réseau SSID: %s et MAC address: %s " % (pkt.info, pkt.addr2)
-    if pkt.type == 0 and pkt.subtype == 0xB:
+            print("APmAc", APmac)
+    if pkt.type == 0 and pkt.subtype == 0xB and len(Clientmac) == 0:
         Clientmac = bytes(pkt.addr2, "utf-8")
-        print(type(Clientmac))
-        # ap_list.append((pkt.ssid, pkt.addr2))
-        # print("Réseau SSID:  et MAC address: %s " % ( pkt.addr2))
         # ssid = pkt.info.decode()
         # APmac = a2b_hex(pkt.addr2)
         print("MAC address client %s " % pkt.addr1)
         print("MAC address client %s " % pkt.addr3)
-        # print(int(ord(pkt[Dot11Elt:3].info)))
-        # return "Réseau SSID: %s et MAC address: %s " % (pkt.info, pkt.addr2)
-    if pkt.haslayer(EAPOL):
-        print(pkt.summary())
-        ANonce = pkt[EAPOL].load[13:13 + 0x20]
-        print(pkt[EAPOL].load[13:13 + 0x20].hex())
-        client_mic = pkt[EAPOL].load[77:77 + 16]
-        print(client_mic.hex())
 
+    if pkt.haslayer(EAPOL):
+        print("Frame number : ", cpt)
+        print(pkt.summary())
+        if cpt == 0:
+            ANonce = pkt[EAPOL].load[13:13 + 0x20]
+            print("ANonce", pkt[EAPOL].load[13:13 + 0x20].hex())
+        if cpt == 1:
+            SNonce = pkt[EAPOL].load[13:13 + 0x20]
+            print("SNonce", pkt[EAPOL].load[13:13 + 0x20].hex())
+            client_mic = pkt[EAPOL].load[77:77 + 16]
+            print("Client mic", client_mic.hex())
+        if cpt == 3:
+            mic_to_test = pkt[EAPOL].load[77:77 + 16]
+            print("MIC To test", mic_to_test)
+        cpt = cpt + 1
 # Authenticator and Supplicant Nonces
-#ANonce = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+# ANonce = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+#SNonce = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+#mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
 B = min(APmac, Clientmac) + max(APmac, Clientmac) + min(ANonce, SNonce) + max(ANonce,
                                                                               SNonce)  # used in pseudo-random function
 
@@ -104,16 +108,16 @@ print("\n\nValues used to derivate keys")
 print("============================")
 print("Passphrase: ", passPhrase, "\n")
 print("SSID: ", ssid, "\n")
-#print("AP Mac: ", b2a_hex(APmac), "\n")
+# print("AP Mac: ", b2a_hex(APmac), "\n")
 print("AP Mac: ", APmac, "\n")
-#print("CLient Mac: ", b2a_hex(Clientmac), "\n")
+# print("CLient Mac: ", b2a_hex(Clientmac), "\n")
 print("CLient Mac: ", Clientmac, "\n")
 print("AP Nonce: ", b2a_hex(ANonce), "\n")
 print("Client Nonce: ", b2a_hex(SNonce), "\n")
 
 # calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
 passPhrase = str.encode(passPhrase)
-#ssid = str.encode(ssid)
+# ssid = str.encode(ssid)
 pmk = pbkdf2(hashlib.sha1, passPhrase, ssid, 4096, 32)
 
 # ap_list.append((pkt.ssid, pkt.addr2))
