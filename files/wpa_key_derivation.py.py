52454dc88a1b9aa96778c6c4d8cf17643a27d02e
rya-sge <ryan.sauge@heig-vd.ch>
2022-05-05 21:49:54 +0200

    clean wpa key derivation

--- files/wpa_key_derivation.py.py
+++ files/wpa_key_derivation.py.py
@@ -24,10 +24,8 @@ __status__ = "Prototype"
 
 from scapy.all import *
 from binascii import a2b_hex, b2a_hex
-# from pbkdf2 import pbkdf2_hex
 from pbkdf2 import *
 import hmac, hashlib
-
 
 def customPRF512(key, A, B):
     """
@@ -49,11 +47,8 @@ wpa = rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase = "actuelle"
 A = "Pairwise key expansion"  # this string is used in the pseudo-random function
-# ssid        = "SWI"
 ssid = ""
-# APmac       = a2b_hex("cebcc8fdcab7")
 APmac = ""
-# Clientmac   = a2b_hex("0013efd015bd")
 Clientmac = ""
 print(len(wpa))
 ANonce = ""
@@ -91,13 +86,10 @@ for pkt in wpa:
             mic_to_test = pkt[EAPOL].load[77:77 + 16]
             print("MIC To test", mic_to_test)
         cpt = cpt + 1
-# Authenticator and Supplicant Nonces
-# ANonce = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-#SNonce = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-#mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
 B = min(APmac, Clientmac) + max(APmac, Clientmac) + min(ANonce, SNonce) + max(ANonce,
                                                                               SNonce)  # used in pseudo-random function
 
