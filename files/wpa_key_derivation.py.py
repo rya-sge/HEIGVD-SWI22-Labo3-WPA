c89ece64a4549049d17c3f22ae3e6e0ca8f018cf
rya-sge <ryan.sauge@heig-vd.ch>
2022-05-05 21:47:45 +0200

    clean wpa key derivation

--- files/wpa_key_derivation.py.py
+++ files/wpa_key_derivation.py.py
@@ -8,15 +8,14 @@ Calcule un MIC d'authentification (le MIC pour la transmission de données
 utilise l'algorithme Michael. Dans ce cas-ci, l'authentification, on utilise
 sha-1 pour WPA2 ou MD5 pour WPA)
 """
-from pstats import Stats
 
-from scapy.layers.dot11 import Dot11Elt, Dot11, Dot11Beacon, Dot11ProbeResp
+from scapy.layers.dot11 import Dot11Beacon, Dot11ProbeResp
 from scapy.layers.eap import EAPOL
 
 from files.pbkdf2 import pbkdf2
-import numpy as np
 
-__author__ = "Abraham Rubinstein et Yann Lederrey"
+
+__author__ = "Abraham Rubinstein, Yann Lederrey, Sauge Ryan et Le Ray Quentin"
 __copyright__ = "Copyright 2017, HEIG-VD"
 __license__ = "GPL"
 __version__ = "1.0"
@@ -27,8 +26,6 @@ from scapy.all import *
 from binascii import a2b_hex, b2a_hex
 # from pbkdf2 import pbkdf2_hex
 from pbkdf2 import *
-from numpy import array_split
-from numpy import array
 import hmac, hashlib
 
 
@@ -63,32 +60,33 @@ ANonce = ""
 cpt = 0
 mic_to_test = ""
 SNonce = ""
-#getlayer(Raw).load)[202:234]
 for pkt in wpa:
+    # GET ssid and AP MAC
     if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
         if pkt.type == 0 and pkt.subtype == 8:
             ssid = pkt.info
             print("SSID" , ssid)
             APmac = bytes(pkt.addr2, "utf-8")
             print("APmAc", APmac)
+    # GET MAC client and verify AP MAC
     if pkt.type == 0 and pkt.subtype == 0xB and len(Clientmac) == 0:
         Clientmac = bytes(pkt.addr1, "utf-8")
-        # ssid = pkt.info.decode()
-        # APmac = a2b_hex(pkt.addr2)
         print("MAC address client %s " % pkt.addr1)
         print("AP address %s " % pkt.addr3)
 
     if pkt.haslayer(EAPOL):
         print("Frame number : ", cpt)
-        print(pkt.summary())
+        # GET ANonce
         if cpt == 0:
             ANonce = pkt[EAPOL].load[13:13 + 0x20]
             print("ANonce", pkt[EAPOL].load[13:13 + 0x20].hex())
+        # GET SNonce and client mic
         if cpt == 1:
             SNonce = pkt[EAPOL].load[13:13 + 0x20]
             print("SNonce", pkt[EAPOL].load[13:13 + 0x20].hex())
             client_mic = pkt[EAPOL].load[77:77 + 16]
             print("Client mic", client_mic.hex())
+        # GET MIC
         if cpt == 3:
             mic_to_test = pkt[EAPOL].load[77:77 + 16]
             print("MIC To test", mic_to_test)
@@ -110,24 +108,16 @@ print("\n\nValues used to derivate keys")
 print("============================")
 print("Passphrase: ", passPhrase, "\n")
 print("SSID: ", ssid, "\n")
-# print("AP Mac: ", b2a_hex(APmac), "\n")
 print("AP Mac: ", APmac, "\n")
-# print("CLient Mac: ", b2a_hex(Clientmac), "\n")
 print("CLient Mac: ", Clientmac, "\n")
 print("AP Nonce: ", b2a_hex(ANonce), "\n")
 print("Client Nonce: ", b2a_hex(SNonce), "\n")
 
 # calculate 4096 rounds to obtain the 256 bit (32 oct) PMK
 passPhrase = str.encode(passPhrase)
-# ssid = str.encode(ssid)
+
 pmk = pbkdf2(hashlib.sha1, passPhrase, ssid, 4096, 32)
 
-# ap_list.append((pkt.ssid, pkt.addr2))
-# print("Réseau SSID: %s et MAC address: %s " % (wpa.info, wpa.addr2))
-# print(int(ord(wpa[Dot11Elt:3].info)))
-
-
-# print (Stats)
 # expand pmk to obtain PTK
 ptk = customPRF512(pmk, str.encode(A), B)
 
